9deb524e3b6ddce165fce44748a845fe7c1ac873
Thaumy <Studio_ai@outlook.com>
2022-02-21 21:32:28 +0800

    v5.0.1
    improved auto resume
    auto start server

--- idb5-ui/vue.config.js
+++ idb5-ui/vue.config.js
@@ -1,10 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
+    transpileDependencies: true,
 
-  pluginOptions: {
-    vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
+    pluginOptions: {
+        vuetify: {
+            // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+        }
+    },
+    //改变打包后文件索引方式
+    publicPath: './'
 })
