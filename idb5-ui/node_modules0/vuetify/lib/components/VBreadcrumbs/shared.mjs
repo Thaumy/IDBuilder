b17721e4be0683b64398d4f46a169129793a3e09
Thaumy <Studio_ai@outlook.com>
2022-03-20 14:37:46 +0800

    ~

--- idb5-ui/node_modules0/vuetify/lib/components/VBreadcrumbs/shared.mjs
+++
@@ -1,2 +0,0 @@
-export const VBreadcrumbsSymbol = Symbol.for('vuetify:breadcrumbs');
-//# sourceMappingURL=shared.mjs.map
--- idb5-ui/node_modules0/vuetify/lib/components/VCard/VCardHeader.mjs
+++
@@ -1,3 +0,0 @@
-import { createSimpleFunctional } from "../../util/index.mjs";
-export const VCardHeader = createSimpleFunctional('v-card-header');
-//# sourceMappingURL=VCardHeader.mjs.map
