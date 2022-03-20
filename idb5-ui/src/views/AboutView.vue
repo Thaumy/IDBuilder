908c339921d819701cb3f986ae74aee85f2ba7bf
Thaumy <Studio_ai@outlook.com>
2022-03-20 14:38:00 +0800

    Squashed commit of the following:
    commit 19c77aee1c8fb3066d307d04902fd5531692b84c
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Sun Mar 20 14:36:01 2022 +0800
    
        ~
    
    commit b8606792d92559a1cefbda471116bbda0db572c2
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Sun Mar 20 13:17:16 2022 +0800
    
        git err
    
    commit 5bed248851d99a1eb8690497246833cfa1cffcb1
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Sun Mar 20 13:06:08 2022 +0800
    
        fixed bugs
    
        fixed unable to find server exec bug
        fixed mdi fonts lost bug
    
    commit c2beeca8c39cbd7388a63ae775214dc1d69b5e92
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Sun Feb 20 20:14:47 2022 +0800
    
        finished CryptoView
    
    commit bcf77d808d6d84722991ae3e97a09d406a5f5dcc
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Thu Feb 17 23:30:35 2022 +0800
    
        finished id,time,hash and string
    
    commit d5a9a994765cd091fd98b140357e67ddb794701a
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Thu Feb 17 09:44:16 2022 +0800
    
        ~
    
    commit 52529d3628144df6c5c37f35494c87fad4e1fa4e
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Wed Feb 16 22:58:21 2022 +0800
    
        fixed websocket resume bugs
    
    commit 3dbbcc972157f38b5f9fe38c23a2caf1d0df51ae
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Wed Feb 16 09:24:35 2022 +0800
    
        ~
    
    commit f9f30e12f9c226587ce0af220462450dbced2ac4
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Tue Feb 15 21:59:57 2022 +0800
    
        ~
    
    commit 7706cb737e0dc63012fd9b3e7533e62cda5bdcad
    Merge: 104d7c8a 9f56e772
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Tue Feb 15 09:01:27 2022 +0800
    
        Merge branch 'dev' of https://github.com/Thaumy/IDBuilder into dev
    
    commit 104d7c8a8c1a7bd9510c7daa25e398730ae014ea
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Tue Feb 15 09:01:24 2022 +0800
    
        updated ui
    
    commit 9f56e772b284d13728a9fac9f38e1af759867c5b
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Sat Feb 12 13:18:28 2022 +0800
    
        Update README.md
    
    commit c9cfb60520737000d5e69cc5bd5e4573ed85b9c6
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Sat Feb 12 13:15:54 2022 +0800
    
        ui ready

---
+++ idb5-ui/src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card style="margin-top: 100px" class="mx-auto" max-width="310" elevation="4">
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="overline mb-4">IDBuilder</div>
+        <v-list-item-title class="headline mb-1">ver 5.0.2</v-list-item-title>
+        <v-list-item-subtitle>This App under MIT License by Thaumy.</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-card-actions>
+      <v-btn target="_blank" href="https://www.thaumy.cn/">About Me</v-btn>
+      <v-btn target="_blank" href="https://github.com/Thaumy/IDBuilder">GitHub Repo</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "AboutView",
+};
+</script>
