d66f1c85d288da12f9ec787b5a7e4d43636ca654
Thaumy <Studio_ai@outlook.com>
2022-04-28 21:40:38 +0800

    dark mode available

--- idb5-ui/src/App.vue
+++ idb5-ui/src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <v-app>
-    <v-main>
-      <v-item-group mandatory selected-class="bg-primary">
-        <v-row style="margin: 0">
-          <v-col
-              v-for="it in tabs"
-              :key="it.title"
-              cols="2"
-          >
-            <v-item v-slot="{ selectedClass, toggle }">
-              <v-card
-                  :class="['justify-center d-flex align-center', selectedClass]"
-                  @click="toggle"
-                  height="30"
-                  :to="it.path"
-              >{{ it.title }}
-              </v-card>
-            </v-item>
-          </v-col>
-        </v-row>
-      </v-item-group>
-      <router-view class="px-2"/>
-
-    </v-main>
+  <v-app style="height: 1000px;">
+    <v-item-group mandatory selected-class="bg-primary">
+      <v-row style="margin: 0">
+        <v-col
+            v-for="it in tabs"
+            :key="it.title"
+            cols="2"
+        >
+          <v-item v-slot="{ selectedClass, toggle }">
+            <v-card
+                :class="['justify-center d-flex align-center', selectedClass]"
+                @click="toggle"
+                height="30"
+                :to="it.path"
+            >{{ it.title }}
+            </v-card>
+          </v-item>
+        </v-col>
+      </v-row>
+    </v-item-group>
+    <router-view class="px-2"/>
   </v-app>
 </template>
 
