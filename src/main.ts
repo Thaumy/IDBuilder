65ace13fce82add1fa4bd938ac0021d3327a2474
Thaumy <thaumy@outlook.com>
2022-12-23 19:44:36 +0800

    migrate ui

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
+import {createApp} from "vue"
+import "./style.css"
+import App from "./App.vue"
 
-createApp(App).mount("#app");
+import vuetify from '@/plugins/vuetify'
+import router from '@/plugins/router'
+
+createApp(App)
+    .use(vuetify)
+    .use(router)
+    .mount('#app')
