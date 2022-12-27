e06bf04bacc7c0c7dcead9a9a034b5942294db81
Thaumy <thaumy@outlook.com>
2022-12-27 23:27:02 +0800

    Squashed commit of the following:
    commit 57e13cde722cf8bb7c243c931910562e07b25753
    Author: Thaumy <thaumy@outlook.com>
    Date:   Tue Dec 27 23:24:29 2022 +0800
    
        IdView and CryptoView accomplished
    
    commit 081124721d00547d7a866f57b73970d79c92d32e
    Author: Thaumy <thaumy@outlook.com>
    Date:   Tue Dec 27 08:01:45 2022 +0800
    
        HashView accomplished
    
    commit e57b0e2a64e1b11079834f68557c29fd33f7755e
    Author: Thaumy <thaumy@outlook.com>
    Date:   Tue Dec 27 07:44:49 2022 +0800
    
        TimeView accomplished
    
    commit eaa80d00c268d39e8d20fc13c92085696f2faff7
    Author: Thaumy <thaumy@outlook.com>
    Date:   Tue Dec 27 00:56:30 2022 +0800
    
        EncodingView accomplished
    
    commit 7368964e8375747b3448fe7f7ecf463efbbb9d1a
    Author: Thaumy <thaumy@outlook.com>
    Date:   Sat Dec 24 14:08:08 2022 +0800
    
        test tauri command
    
    commit b5fa9c7601a03153a29e12d40df5c9d91b8cb84f
    Author: Thaumy <thaumy@outlook.com>
    Date:   Sat Dec 24 09:49:13 2022 +0800
    
        resize window
    
    commit da518a1e321dd46147af84953a108cb519b9ce94
    Author: Thaumy <thaumy@outlook.com>
    Date:   Fri Dec 23 23:59:54 2022 +0800
    
        beautify UI
    
    commit 2b85cb53490676da7897f4cbb1d814f729ba24dc
    Author: Thaumy <thaumy@outlook.com>
    Date:   Fri Dec 23 19:44:36 2022 +0800
    
        migrate ui
    
    commit 765ae462ce676ee53309423775a6191336a3d42c
    Author: Thaumy <thaumy@outlook.com>
    Date:   Tue Dec 20 21:28:29 2022 +0800
    
        just init

---
+++ src/components/TimeView.vue
@@ -0,0 +1,122 @@
+<template>
+
+  <div>
+    <v-row class="mb-n6">
+      <v-col cols="6">
+        <v-text-field
+            hide-details
+            outlined
+            label="Sec Timestamp"
+            prepend-inner-icon="mdi-alpha-s-box"
+            v-model="sec"
+        />
+      </v-col>
+      <v-col cols="6">
+        <v-text-field
+            hide-details
+            outlined
+            label="Millisec Timestamp"
+            prepend-inner-icon="mdi-alpha-m-box"
+            v-model="milli"
+        />
+      </v-col>
+    </v-row>
+    <v-row class="mb-n6">
+      <v-col cols="6">
+        <v-text-field
+            hide-details
+            outlined
+            label="ISO8601 Time Format"
+            prepend-inner-icon="mdi-alpha-i-box"
+            v-model="iso8601"
+        />
+      </v-col>
+      <v-col cols="6">
+        <v-text-field
+            hide-details
+            disabled
+            outlined
+            label="-"
+        />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="6">
+        <v-text-field
+            hide-details
+            disabled
+            outlined
+            label="-"
+        />
+      </v-col>
+      <v-col cols="6">
+        <v-text-field
+            hide-details
+            disabled
+            outlined
+            label="-"
+        />
+      </v-col>
+    </v-row>
+
+    <v-row justify="center" class="mt-2">
+      <v-col cols="2">
+        <v-text-field
+            hide-details
+            label="UTC+"
+            type="number"
+            v-model="offset"
+            :error="offset_err"
+        />
+      </v-col>
+    </v-row>
+
+    <v-row justify="center">
+      <v-btn
+          class="position-absolute"
+          style="bottom: 40px"
+          width="90%"
+          height="60px"
+          v-on:click="generate()"
+      >GENERATE
+      </v-btn>
+    </v-row>
+  </div>
+
+</template>
+
+<script lang="ts" setup>
+
+import {ref} from "vue"
+import {invoke} from "@tauri-apps/api/tauri"
+
+const sec = ref("")
+const milli = ref("")
+const iso8601 = ref("")
+
+const offset = ref("0")
+const offset_err = ref(false)
+
+const data = ref({})
+
+async function generate() {
+  //检查合法性
+  if (Number(offset.value) < -12 || Number(offset.value) > 12) {
+    offset_err.value = true
+  } else {
+    offset_err.value = false
+
+    try {
+      let result = <any>await invoke('time_generate', {offset: offset.value})
+      sec.value = result.sec
+      milli.value = result.milli
+      iso8601.value = result.iso8601
+    } catch (e) {
+      sec.value = <string>e
+      milli.value = <string>e
+      iso8601.value = <string>e
+    }
+  }
+}
+
+</script>
