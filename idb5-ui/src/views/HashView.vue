a6a084fa5ca42b339818db13e2e45f3ca2d7baf5
Thaumy <thaumy@outlook.com>
2022-12-28 17:15:08 +0800

    Squashed commit of the following:
    commit de81f3f91de99bec1c182cf0356d2975651da45e
    Author: Thaumy <thaumy@outlook.com>
    Date:   Wed Dec 28 17:14:43 2022 +0800
    
        update doc
    
    commit 852dc008e9ceadf3264fd47338ac6451f5fec387
    Author: Thaumy <thaumy@outlook.com>
    Date:   Sat Jul 30 22:32:00 2022 +0800
    
        Update README.md
    
    commit a7309380865582f0c559bd1e998ad0b891ff6d66
    Author: Thaumy <thaumy@outlook.com>
    Date:   Sat Jul 30 22:28:33 2022 +0800
    
        Update README.md
    
    commit 6f47fc5d597c201fc4700103c9f1d78e75c59644
    Author: Thaumy <thaumy@outlook.com>
    Date:   Sat Jul 30 22:26:58 2022 +0800
    
        Update README.md
    
    commit df576abc6cfc51dbd1ec5416f3e8a4d5bedc415a
    Author: Thaumy <thaumy@outlook.com>
    Date:   Sat Jul 30 22:16:39 2022 +0800
    
        updated ui : ver 5.0.2 -> ver 5.1.0
    
    commit 9fb4f2aeb1dc00e83c63ab9df0b4a5addebf1ff6
    Author: Thaumy <thaumy@outlook.com>
    Date:   Sat Jul 30 22:11:54 2022 +0800
    
        updated to palaflake v1.1.0
    
    commit d059ba0f473c23aa6f0b7e899e2dc3056e54b2a3
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Thu Apr 28 16:45:55 2022 +0800
    
        dark mode theme
    
    commit 55c6c97aa881e262a39022d9bc955dbd707d448e
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Sun Mar 20 14:36:01 2022 +0800
    
        ~
    
    commit 6578d31d2aeceae4a5390fff0e128708a4be9ca3
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Sun Mar 20 13:17:16 2022 +0800
    
        git err
    
    commit 19871b6066ee98c06369a23510fb2fe16281bcc6
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Sun Mar 20 13:06:08 2022 +0800
    
        fixed bugs
    
        fixed unable to find server exec bug
        fixed mdi fonts lost bug
    
    commit eef1607a5e74079814f87085f8a09aa5df1bd5ad
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Sun Feb 20 20:14:47 2022 +0800
    
        finished CryptoView
    
    commit 051566fa9b6f65531bd2ebe9ec428d24a0c9774e
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Thu Feb 17 23:30:35 2022 +0800
    
        finished id,time,hash and string
    
    commit 72fb2cdd66ac9a828ccd0a2fccd1df1c4589d818
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Thu Feb 17 09:44:16 2022 +0800
    
        ~
    
    commit 302e0194d187c81d19821e6a01d55b68e2ec7ce1
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Wed Feb 16 22:58:21 2022 +0800
    
        fixed websocket resume bugs
    
    commit d4106f4a3ebd5cacd016b38ca58f6c655a6028e4
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Wed Feb 16 09:24:35 2022 +0800
    
        ~
    
    commit 601bc4b4bb37bef4446920d4a58759611c247426
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Tue Feb 15 21:59:57 2022 +0800
    
        ~
    
    commit 781da5a094772a7fe672972a7ac48d03295df214
    Merge: 481ece87 b5de9967
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Tue Feb 15 09:01:27 2022 +0800
    
        Merge branch 'dev' of https://github.com/Thaumy/IDBuilder into dev
    
    commit 481ece87aa1a940eb570683680fb7e66dc05ac1d
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Tue Feb 15 09:01:24 2022 +0800
    
        updated ui
    
    commit b5de99674100f345be7189a0555c426021d2c37e
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Sat Feb 12 13:18:28 2022 +0800
    
        Update README.md
    
    commit 3a53d5dc1ec88b73421b50d207e78544660bc4c4
    Author: Thaumy <Studio_ai@outlook.com>
    Date:   Sat Feb 12 13:15:54 2022 +0800
    
        ui ready

---
+++ idb5-ui/src/views/HashView.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <v-row class="mb-n8">
+      <v-col cols="12">
+        <v-textarea
+            class="mb-n2"
+            rows="6"
+            label="Text"
+            prepend-inner-icon="mdi-alpha-t-box"
+            v-model="text"
+        ></v-textarea>
+      </v-col>
+    </v-row>
+
+    <v-radio-group v-model="hash_algh">
+      <v-row class="mb-n8" justify="center">
+        <v-col cols="2">
+          <v-radio label="MD5" value="md5"></v-radio>
+        </v-col>
+        <v-col cols="2">
+          <v-radio label="SHA1" value="sha1"></v-radio>
+        </v-col>
+        <v-col cols="2">
+          <v-radio label="SHA256" value="sha256"></v-radio>
+        </v-col>
+      </v-row>
+    </v-radio-group>
+
+    <v-row class="mb-8" justify="center">
+      <v-btn width="50%" v-on:click="regen_btn()">ENCRYPT</v-btn>
+    </v-row>
+    <v-row justify="center">
+      <v-col>
+        <v-text-field
+            label="Hash"
+            prepend-inner-icon="mdi-alpha-h-box"
+            v-model="hash"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HashView",
+  data() {
+    return {
+      text: "",
+      hash: "",
+      hash_algh: "md5",
+
+      data: {}
+    };
+  },
+  methods: {
+    render() {
+      this.hash = this.data.hash
+    },
+    regen_btn() {
+      let base64str = btoa(this.text)
+
+      this.$ws.onmessage = (msg) => {
+        console.log(msg.data)
+        this.data = JSON.parse(msg.data)
+        this.render()
+      }
+
+      this.$ws.send(`get_hash_view_data ${this.hash_algh} ${base64str}`)
+    },
+  }
+};
+</script>
