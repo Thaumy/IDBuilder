e362beefce45ae18c14c1055d09d14325e484ff3
Thaumy <thaumy@outlook.com>
2022-12-28 17:14:43 +0800

    update doc

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import {createApp} from "vue"
+import "./style.styl"
+import App from "./App.vue"
+
+import vuetify from './plugins/vuetify'
+import router from './plugins/router'
+
+createApp(App)
+    .use(vuetify)
+    .use(router)
+    .mount('#app')
