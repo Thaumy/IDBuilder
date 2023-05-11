64b8d87ec5211ac3599ba1060cd5b8fd71268045
Thaumy <thaumy@outlook.com>
2023-05-11 15:27:53 +0800

    Squashed commit of the following:
    commit 329a8c0cdf4c73a8143f2651f4afe041352837d3
    Author: Thaumy <thaumy@outlook.com>
    Date:   Thu May 11 15:25:49 2023 +0800
    
        update deps
    
    commit 033929157701559e2d6e1b41596886fa9b69ff8a
    Author: Thaumy <thaumy@outlook.com>
    Date:   Wed Jan 11 19:19:20 2023 +0800
    
        update doc
    
    commit de9d8ebe180afc321760dadb4c157acfb27d585e
    Author: Thaumy <thaumy@outlook.com>
    Date:   Wed Jan 11 19:00:20 2023 +0800
    
        update version
    
    commit a454d8b5b4d390348cc36b6f7e4cbe6144d746ad
    Author: Thaumy <thaumy@outlook.com>
    Date:   Wed Jan 11 18:56:18 2023 +0800
    
        support BCrypt hashing
    
    commit 520e83161f7a498c3146e256dcb6895fab1edcd1
    Author: Thaumy <thaumy@outlook.com>
    Date:   Mon Jan 2 11:39:55 2023 +0800
    
        update doc
    
    commit b04f62af765ea8c68f9874e947f25c9fdb64807c
    Author: Thaumy <thaumy@outlook.com>
    Date:   Sun Jan 1 13:16:01 2023 +0800
    
        update version
    
    commit e60288f8c30890610560662b7c8530fc23c2b689
    Author: Thaumy <thaumy@outlook.com>
    Date:   Sun Jan 1 10:00:54 2023 +0800
    
        simplified with ruster
    
    commit cac88097230723ecdef638b03744e7a4f134c4da
    Author: Thaumy <thaumy@outlook.com>
    Date:   Thu Dec 29 09:35:46 2022 +0800
    
        Update package.json
    
    commit 80899ed56ba8cc96fc0da97b5cd4727a0b109d7c
    Author: Thaumy <thaumy@outlook.com>
    Date:   Thu Dec 29 09:30:59 2022 +0800
    
        Update AboutView.vue
    
    commit 41150bde5ebe30f5f8399d19e7ac3a867a36f3ae
    Author: Thaumy <thaumy@outlook.com>
    Date:   Thu Dec 29 09:27:44 2022 +0800
    
        update tauri target
    
    commit 0fc3ca9f86d42f757c30ab068122524f859aec05
    Author: Thaumy <thaumy@outlook.com>
    Date:   Wed Dec 28 17:24:03 2022 +0800
    
        update doc
    
    commit 57e13cde722cf8bb7c243c931910562e07b25753
    Author: Thaumy <thaumy@outlook.com>
    Date:   Tue Dec 27 23:24:29 2022 +0800
    
        IdView and CryptoView accomplished
    
    commit 081124721d00547d7a866f57b73970d79c92d32e
    Author: Thaumy <thaumy@outlook.com>
    Date:   Tue Dec 27 08:01:45 2022 +0800
    
        HashView accomplished
    
    commit e57b0e2a64e1b11079834f68557c29fd33f7755e
    Author: Thaumy <thaumy@outlook.com>
    Date:   Tue Dec 27 07:44:49 2022 +0800
    
        TimeView accomplished
    
    commit eaa80d00c268d39e8d20fc13c92085696f2faff7
    Author: Thaumy <thaumy@outlook.com>
    Date:   Tue Dec 27 00:56:30 2022 +0800
    
        EncodingView accomplished
    
    commit 7368964e8375747b3448fe7f7ecf463efbbb9d1a
    Author: Thaumy <thaumy@outlook.com>
    Date:   Sat Dec 24 14:08:08 2022 +0800
    
        test tauri command
    
    commit b5fa9c7601a03153a29e12d40df5c9d91b8cb84f
    Author: Thaumy <thaumy@outlook.com>
    Date:   Sat Dec 24 09:49:13 2022 +0800
    
        resize window
    
    commit da518a1e321dd46147af84953a108cb519b9ce94
    Author: Thaumy <thaumy@outlook.com>
    Date:   Fri Dec 23 23:59:54 2022 +0800
    
        beautify UI
    
    commit 2b85cb53490676da7897f4cbb1d814f729ba24dc
    Author: Thaumy <thaumy@outlook.com>
    Date:   Fri Dec 23 19:44:36 2022 +0800
    
        migrate ui
    
    commit 765ae462ce676ee53309423775a6191336a3d42c
    Author: Thaumy <thaumy@outlook.com>
    Date:   Tue Dec 20 21:28:29 2022 +0800
    
        just init

--- src/components/AboutView.vue
+++ src/components/AboutView.vue
@@ -3,7 +3,7 @@
   <v-card class="mx-auto mt-16 py-5 px-6" elevation="4">
     <v-list-item three-line>
       <div class="overline mb-4 font-weight-bold">IDBuilder</div>
-      <v-list-item-title class="headline mb-1">ver 6.0.1</v-list-item-title>
+      <v-list-item-title class="headline mb-1">ver 6.0.3</v-list-item-title>
       <v-list-item-subtitle>This APP under MIT license<br> Maintained by Thaumy.</v-list-item-subtitle>
     </v-list-item>
 
